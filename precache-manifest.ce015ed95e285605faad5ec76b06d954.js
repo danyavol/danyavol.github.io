9ccf86c5692acaeeb99c88b32f095e391eb7db75
Volosyuk Daniil <danya.volosyuk@gmail.com>
2020-05-14 16:09:43 +0300

    Updates

---
+++ precache-manifest.ce015ed95e285605faad5ec76b06d954.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17efc395371c18c9fbf43134964c503",
+    "url": "/systems-project/index.html"
+  },
+  {
+    "revision": "2f6802504e480f22c944",
+    "url": "/systems-project/static/css/2.08a21236.chunk.css"
+  },
+  {
+    "revision": "0e8d97cf945de6d14d9b",
+    "url": "/systems-project/static/css/main.eb2ed1bc.chunk.css"
+  },
+  {
+    "revision": "2f6802504e480f22c944",
+    "url": "/systems-project/static/js/2.ca8f8dc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/systems-project/static/js/2.ca8f8dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8d97cf945de6d14d9b",
+    "url": "/systems-project/static/js/main.c636b114.chunk.js"
+  },
+  {
+    "revision": "f1519d3904da9e76df99",
+    "url": "/systems-project/static/js/runtime-main.325b62ba.js"
+  },
+  {
+    "revision": "400cc061bc4254b117c10a6d5e04be92",
+    "url": "/systems-project/static/media/background.400cc061.jpg"
+  }
+]);
